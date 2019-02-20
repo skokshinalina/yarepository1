b0e7092eb954b1c8eb351f90c3087d59c414c83a
skokshinalina <a.skokshina@yandex.ru>
2019-02-21 00:59:45 +0700

    1prakt

---
+++ 02_sum.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main (void)
+{
+int r, p;
+scanf ("%d%d", &r, &p);
+printf ("%d\n", r+p);
+return 0;
+}
---
+++ 03_pow.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <math.h>
+
+int main (void)
+{
+int s, w;
+float k;
+scanf ("%d%d", &s, &w);
+k = pow ((double) s, (double) w);
+printf ("%f" , k);
+return 0;
+}
+
